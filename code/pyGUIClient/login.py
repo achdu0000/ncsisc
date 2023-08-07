8cb8afa2b14489832cbc83b332ccf779df441b67
achdu0000 <achdu0000@163.com>
2023-08-07 09:07:59 +0800

    update login.py and main.py

--- code/pyGUIClient/login.py
+++ code/pyGUIClient/login.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 import hashlib
 from PIL import Image, ImageTk
+from tkinter import messagebox
 
 
 def calc_sha256(string):
@@ -18,7 +19,7 @@ class Login:
         screen_width,
         screen_height,
         window_width=800,
-        window_height=600,
+        window_height=550,
     ):
         self.username_password_table = username_password_table
         self.server_ip_list = server_ip_list
@@ -38,9 +39,11 @@ class Login:
                 username not in self.username_password_table
                 or self.username_password_table[username] != password
             ):
-                error_label.config(text="Invalid username or password!")
+                messagebox.showinfo('错误提示', 'Invalid username or password!')
+                #error_label.config(text="Invalid username or password!")
             elif server_ip not in self.server_ip_list:
-                error_label.config(text="Invalid server IP!")
+                messagebox.showinfo('错误提示', 'Invalid server IP!')
+                #error_label.config(text="Invalid server IP!")
             else:
                 # 修改状态、用户名和IP
                 state_user_ip[0] = True
@@ -50,7 +53,13 @@ class Login:
                 window.destroy()
 
         state_user_ip = [False, None, None]
+        '''
+        # 创建一个Style实例
+        style = ttk.Style()
 
+        # 设置框架的背景颜色为浅灰色
+        style.configure("Custom.TFrame", background="light gray")
+        '''
         # 创建登录窗口
         window = tk.Tk()
         window.title("LOG IN")
@@ -61,8 +70,8 @@ class Login:
         y = int((self.screen_height - self.window_height) / 2)
         # 设置窗口的位置
         window.geometry(f"+{x}+{y}")
-
-        # 加载背景图片并进行缩放
+        
+        # # 加载背景图片并进行缩放
         bg_image = Image.open("pic/bg.png")
         bg_image = bg_image.resize(
             (self.window_width, self.window_height), Image.LANCZOS
@@ -71,7 +80,100 @@ class Login:
         # 设置背景图片
         background_label = tk.Label(image=bg_photo_image)
         background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        
 
+
+
+        # bg="#f2f6fa"
+        bg="#e5f0f6"
+        # Create login frame
+        frame_width = 20
+        self.style = ttk.Style()
+        self.style.configure("Custom.TFrame", background=bg)
+        self.login_frame = ttk.Frame(window,padding=10,style="Custom.TFrame",width=frame_width)
+        self.login_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+
+        # Frame title label
+        frame_title = ttk.Label(self.login_frame, text="Account Login", font=("Arial", 18, "bold"),background=bg)
+        frame_title.grid(row=0, column=0, columnspan=2, pady=(0, 20))
+
+        # Account icon and label
+        account_icon = Image.open("pic/user1.png")
+        account_icon = account_icon.resize((32, 32), Image.LANCZOS)
+        self.account_photo = ImageTk.PhotoImage(account_icon)
+        account_label = ttk.Label(self.login_frame, text="Username:", background=bg,font=("Segoe UI", 12),image=self.account_photo, compound="left")
+        account_label.grid(row=1, column=0, sticky="w",pady=(0, 10))
+
+        # Password icon and label
+        password_icon = Image.open("pic/key.png")
+        password_icon = password_icon.resize((32, 32), Image.LANCZOS)
+        self.password_photo = ImageTk.PhotoImage(password_icon)
+        password_label = ttk.Label(self.login_frame, text="Password:",background=bg, font=("Segoe UI", 12),image=self.password_photo, compound="left")
+        password_label.grid(row=2, column=0, sticky="w",pady=(0, 10))
+
+        # Server icon and label
+        server_icon = Image.open("pic/IP.png")
+        server_icon = server_icon.resize((32, 32), Image.LANCZOS)
+        self.server_photo = ImageTk.PhotoImage(server_icon)
+        server_label = ttk.Label(self.login_frame, text="Server IP:",background=bg,font=("Segoe UI", 12), image=self.server_photo, compound="left")
+        server_label.grid(row=3, column=0, sticky="w",pady=(0, 20))
+
+
+        # Username entry
+        username_entry = ttk.Entry(self.login_frame,width=17)
+        username_entry.grid(row=1, column=1, padx=8, pady=(0,10), sticky="w")
+        
+     
+        # Password entry
+        password_entry = ttk.Entry(self.login_frame, show="•",width=17)
+        password_entry.grid(row=2, column=1, padx=8, pady=(0,10), sticky="w")
+       
+        # Server IP entry
+        server_ip_entry = ttk.Entry(self.login_frame,width=17)
+        server_ip_entry.grid(row=3, column=1, padx=8, pady=(0,20), sticky="w")
+
+        # 创建样式
+        style = ttk.Style()
+        style.configure("Custom.TButton", font=("Segoe UI", 10))
+        
+        # Login button
+        login_button = ttk.Button(self.login_frame,style="Custom.TButton", text="Login", command=validate_login)
+        login_button.grid(row=5, column=0, columnspan=2, pady=10)
+        
+        # Watermark label
+        watermark_label = ttk.Label(window, text="Copyright © 2023 DuiBuDui Team All Rights Reserved", font=("Arial", 8, "italic"), foreground="gray")
+        watermark_label.place(relx=1, rely=1, anchor=tk.SE)
+        
+        username_entry.insert(0, "admin")
+        password_entry.insert(0, "123456")
+        server_ip_entry.insert(0, "127.0.0.1")
+
+        # 进入登录窗口的主循环
+        window.mainloop()
+
+        return state_user_ip[0], state_user_ip[1], state_user_ip[2]
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    root.destroy()
+    table = {}
+    table[
+        "admin"
+    ] = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92"  # username=admin, password=sha256(123456)
+
+    server_ip_list = ["127.0.0.1"]
+
+    login_window = Login(
+        username_password_table=table,
+        server_ip_list=server_ip_list,
+        screen_width=screen_width,
+        screen_height=screen_height,
+    )
+    state, user, server_ip = login_window.run()
+    '''
         # 创建样式
         style = ttk.Style()
         style.configure("Custom.TLabel", font=("Arial", 14, "bold"), padding=(10, 5))
@@ -110,12 +212,34 @@ class Login:
 
         # 将Frame容器放置在窗口的中间位置
         frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+'''
+'''
+        #账号与密码文字标签
+        username_label = tk.Label(window, text = '账号', bg='lightskyblue', fg='white', font=('Arial', 12), width=7, height=1)
+        username_label.grid(row=0, column=0, sticky="e")
+        #username_label.place(relx=0.29,rely=0.4)
+        password_label = tk.Label(window, text = '密码', bg='lightskyblue', fg='white', font=('Arial', 12), width=7, height=1)
+        password_label.grid(row=1, column=0, sticky="e")
+        #password_label.place(relx=0.29,rely=0.5)
+        server_ip_label = tk.Label(window, text = '服务器IP', bg='lightskyblue', fg='white', font=('Arial', 12), width=7, height=1)
+        #server_ip_label.place(relx=0.29,rely=0.6)
+        server_ip_label.grid(row=2, column=0, sticky="e")
 
-        username_entry.insert(0, "admin")
-        password_entry.insert(0, "123456")
-        server_ip_entry.insert(0, "127.0.0.1")
 
-        # 进入登录窗口的主循环
-        window.mainloop()
-
-        return state_user_ip[0], state_user_ip[1], state_user_ip[2]
+        #账号与密码输入框
+        username_entry = tk.Entry(window,width=20,highlightthickness = 1,highlightcolor = 'lightskyblue',relief='groove')  #账号输入框
+        #username_entry.place(relx=0.4,rely=0.4 )  #添加进主页面,relx和rely意思是与父元件的相对位置
+        username_entry.grid(row=0, column=1, sticky="w")
+        password_entry = tk.Entry(window,show='*',highlightthickness = 1,highlightcolor = 'lightskyblue',relief='groove')  #密码输入框
+        password_entry.grid(row=1, column=1, sticky="w")
+        #password_entry.place(relx=0.4,rely=0.5) #添加进主页面
+        server_ip_entry = tk.Entry(window,width=20,highlightthickness = 1,highlightcolor = 'lightskyblue',relief='groove')  #密码输入框
+        #server_ip_entry.place(relx=0.4,rely=0.6) #添加进主页面
+        server_ip_entry.grid(row=2, column=1, sticky="w")
+        
+        #登录与注册按钮
+        login_button = tk.Button(window,text='登录',font = ('宋体',12),width=4,height=1,command=validate_login,relief='solid',bd = 0.5,bg='lightcyan')
+        #login_button.place(relx=0.41,rely=0.7)
+        login_button.grid(row=3, column=0, columnspan=2, sticky="nsew")
+'''
+        
--- code/pyGUIClient/main.py
+++ code/pyGUIClient/main.py
@@ -6,17 +6,16 @@ from datetime import datetime
 from upload_file import upload_file, recieve_file
 from download_file import download_file
 from show_result import show_result
-import logging
+from PIL import Image, ImageTk
 
 
 def set_log(scrolled_text, string):
     now = datetime.now()
-    msg = now.strftime("[%Y-%m-%d %H:%M:%S] ") + string + '\n'
+    prefix = now.strftime("[%Y-%m-%d %H:%M:%S] ")
     scrolled_text.config(state="normal")
-    scrolled_text.insert(tk.END, msg)
+    scrolled_text.insert(tk.END, prefix + string + "\n")
     scrolled_text.configure(state="disabled")
     scrolled_text.see(tk.END)
-    logging.info(msg)
 
 
 class Main:
@@ -29,7 +28,7 @@ class Main:
         screen_width,
         screen_height,
         window_width=800,
-        window_height=600,
+        window_height=550,
     ):
         self.user = user
         self.server_ip = server_ip
@@ -38,8 +37,6 @@ class Main:
         self.window_width = window_width
         self.window_height = window_height
         self.result_file = None
-
-        logging.basicConfig(filename='pyGUIClient.log',level=logging.INFO)
 
     def run(self):
         def upload():
@@ -83,7 +80,7 @@ class Main:
         style = ttk.Style()
         style.configure("Custom.TLabel", font=("Arial", 14, "bold"), padding=(10, 5))
         style.configure("Custom.TEntry", font=("Arial", 14, "bold"), padding=(10, 5))
-        style.configure("Custom.TButton", font=("Arial", 14, "bold"), padding=(10, 5))
+        style.configure("Custom.TButton", font=("Arial", 12, "bold"), padding=(10, 5),width=15)
         style.configure(
             "Custom.TScrolledText",
             background="white",
@@ -94,6 +91,69 @@ class Main:
 
         frame = tk.Frame(window)
         frame.pack(fill=tk.BOTH, expand=True)
+        '''
+        # 左边部分 - 插入图片
+        left_frame = tk.Frame(frame)
+        left_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        
+        bg_image = Image.open("pic/hh1.png")
+        bg_image = bg_image.resize((self.window_width // 2, self.window_height // 2), Image.LANCZOS)
+        bg_photo_image = ImageTk.PhotoImage(bg_image)
+
+        bg_label = tk.Label(left_frame, image=bg_photo_image)
+        bg_label.pack(anchor="nw")  # 使用anchor参数来设置标签在Frame中的对齐方式
+        '''
+        # 左边部分 - 插入图片
+        left_frame = tk.Frame(frame)
+        left_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        bg_image = Image.open("pic/hh1.png")
+        bg_image = bg_image.resize((self.window_width // 2, self.window_height // 2), Image.LANCZOS)
+        bg_photo_image = ImageTk.PhotoImage(bg_image)
+
+        bg_label = tk.Label(left_frame, image=bg_photo_image)
+        bg_label.pack(side=tk.TOP, anchor="nw")
+
+        # 下半部分 - 放置四个按钮
+        button_frame = tk.Frame(left_frame, pady=20)
+        button_frame.pack(side=tk.TOP, anchor="sw")
+
+        
+        upload_button = ttk.Button(
+            button_frame, text="Upload File", style="Custom.TButton", command=upload
+        )
+        upload_button.grid(row=0, column=0, padx=10, pady=10)
+
+        download_button = ttk.Button(
+            button_frame,
+            text="Download Result",
+            style="Custom.TButton",
+            command=download,
+        )
+        download_button.grid(row=0, column=1, padx=10, pady=10)
+
+        show_result_button = ttk.Button(
+            button_frame, text="Show Result", style="Custom.TButton", command=result
+        )
+        show_result_button.grid(row=1, column=0, padx=10, pady=10)
+
+        exit_button = ttk.Button(
+            button_frame, text="Exit", style="Custom.TButton", command=exit_main
+        )
+        exit_button.grid(row=1, column=1, padx=10, pady=10)
+
+
+        '''
+        # 右边部分 - 滚动文本框
+        right_frame = tk.Frame(frame, width=self.window_width // 2)
+        right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+        '''
+        right_frame = tk.Frame(frame)
+        right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+
+        # 计算相对于窗口宽度的位置
+        rel_width = 0.5  # 相对于窗口宽度的一半
+        right_frame.place(relx=1.0, rely=0, relwidth=rel_width, relheight=1.0, anchor=tk.NE)
 
         # 创建标题标签
         title_label = ttk.Label(frame, text="LOG AREA", style="Custom.TLabel")
@@ -104,7 +164,40 @@ class Main:
         log_text.pack(fill=tk.BOTH, expand=True)
 
         Main.scrolled_text = log_text
+        '''
+        # 创建按钮容器
+        button_frame = tk.Frame(window)
+        button_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
+        # 创建按钮
+        upload_button = ttk.Button(
+            button_frame, text="Upload File", style="Custom.TButton", command=upload
+        )
+        upload_button.grid(row=0, column=0, padx=10, pady=10)
+
+        download_button = ttk.Button(
+            button_frame,
+            text="Download Result",
+            style="Custom.TButton",
+            command=download,
+        )
+        download_button.grid(row=0, column=1, padx=10, pady=10)
+
+        show_result_button = ttk.Button(
+            button_frame, text="Show Result", style="Custom.TButton", command=result
+        )
+        show_result_button.grid(row=1, column=0, padx=10, pady=10)
+
+        exit_button = ttk.Button(
+            button_frame, text="Exit", style="Custom.TButton", command=exit_main
+        )
+        exit_button.grid(row=1, column=1, padx=10, pady=10)
+
+        '''
+
+
+
+        '''
         # 创建按钮容器
         button_frame = tk.Frame(window)
         button_frame.pack(side=tk.BOTTOM, anchor=tk.CENTER)
@@ -132,10 +225,11 @@ class Main:
             button_frame, text="Exit", style="Custom.TButton", command=exit_main
         )
         exit_button.pack(side=tk.LEFT, padx=10, pady=10)
-
+        '''
+        '''
         # 设置窗口宽度与文本框等宽
         window.update_idletasks()
         width = window.winfo_width()
         log_text.configure(width=width)
-
+        '''
         window.mainloop()
